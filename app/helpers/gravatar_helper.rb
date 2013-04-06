426d3cc616bc61ef8600b0df21f3fac79b007f2a
Tom-Eric Gerritsen <tomeric@eet.nu>
2013-04-06 19:07:18 +0200

    Add account information of logged in user to header

---
+++ app/helpers/gravatar_helper.rb
@@ -0,0 +1,18 @@
+    require 'digest/md5'
+module GravatarHelper
+ def gravatar_tag(email, *args)
+    opts = args.extract_options!
+    opts[:class]||=""
+    opts[:class]+=" gravatar"
+    size = opts.delete(:size) || 80
+    
+    default=""
+    if opts[:default]
+      require 'cgi'
+      default= "&d=#{CGI::escape(opts.delete(:default))}"
+    end
+    hash = Digest::MD5.hexdigest(email.downcase)    
+    
+    image_tag "http://www.gravatar.com/avatar/#{hash}?size=#{size}#{default}", opts
+  end
+end
