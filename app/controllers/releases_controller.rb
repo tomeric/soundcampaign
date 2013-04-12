f99f7b16012f0cdfa20a6981aeab3767d8e4c2f9
Tom-Eric Gerritsen <tomeric@eet.nu>
2013-04-12 22:58:38 +0200

    Start of releases admin interface

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -2,4 +2,29 @@ class ReleasesController < ApplicationController
   
   layout 'backend'
   
+  def index
+    @releases = current_user.releases.group_by(&:label)
+  end
+  
+  def new
+    @release = Release.new
+  end
+  
+  def create
+    @release = Release.new(release_params)
+    @release.owner = current_user
+    
+    if @release.save
+      redirect_to releases_url
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+  def release_params
+    params.require(:release)
+  end
+  
 end
