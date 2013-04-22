2c8aa17b506aac64fe5e4a577107287f966c3be8
Tom-Eric Gerritsen <tomeric@eet.nu>
2013-04-22 22:27:42 +0200

    Remove old migrations

--- db/migrate/20130421100916_remove_owner_id_from_artists_labels_and_releases.rb
+++
@@ -1,7 +0,0 @@
-class RemoveOwnerIdFromArtistsLabelsAndReleases < ActiveRecord::Migration
-  def change
-    remove_column :artists,  :owner_id
-    remove_column :labels,   :owner_id
-    remove_column :releases, :owner_id
-  end
-end
