4d76725e757fc0c5b41aa335d6bf45763ba180c2
Tom-Eric Gerritsen <tomeric@eet.nu>
2013-07-23 15:59:26 +0200

    Move bootstrap-select.min.js to vendor/assets/javascripts and include it via the asset pipeline

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 //= require releases
 //= require imports
 //= require release
+//= require bootstrap-select.min
 
 $(function(){
   // $('a').clickover({ html : true, global_close: false });
@@ -18,10 +19,10 @@ $(function(){
   $('textarea').autosize();
   $('.selectpicker').selectpicker();
   $('.release-list-add').on('click', function(){
-  	$(".release-list").append('<div class="release-list-select"><div class="release-list-select-icon"></div><div class="release-list-select-date"><input type="text" value="Select date" class="datepicker_recurring_start" /></div><div class="release-list-select-time"><input type="text" value="12:30" /></div><div class="release-list-select-list"><select class="select selectpicker" title="Select contactlist"><option></option><option>Blue</option><option>Red</option><option>Green</option><option>Yellow</option><option>Brown</option></select></div><div class="release-list-select-del"></div></div>')
-	$('.selectpicker').selectpicker();
-	});
+    $(".release-list").append('<div class="release-list-select"><div class="release-list-select-icon"></div><div class="release-list-select-date"><input type="text" value="Select date" class="datepicker_recurring_start" /></div><div class="release-list-select-time"><input type="text" value="12:30" /></div><div class="release-list-select-list"><select class="select selectpicker" title="Select contactlist"><option></option><option>Blue</option><option>Red</option><option>Green</option><option>Yellow</option><option>Brown</option></select></div><div class="release-list-select-del"></div></div>')
+    $('.selectpicker').selectpicker();
+  });
   $('body').on('focus',".datepicker_recurring_start", function(){
     $(this).datepicker();
-	});
+  });
 });
