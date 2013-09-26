beee837d01597b312be0af73bb9c1424f3c36ab5
Tom-Eric Gerritsen <tomeric@eet.nu>
2013-09-26 16:10:32 +0200

    Small refactoring of CampaignMailer

--- app/mailers/campaign_mailer.rb
+++ app/mailers/campaign_mailer.rb
@@ -3,16 +3,21 @@ class CampaignMailer < ActionMailer::Base
   
   default from: 'info@soundcampaign.com'
   
-  def preview_campaign(campaign, recipients)
+  def preview_campaign(campaign, recipients = [])
+    normal_campaign(campaign, recipients, "[Preview] #{campaign.email_subject}")
+  end
+  
+  def normal_campaign(campaign, recipients = [], subject = nil)
     @campaign = campaign
     @release  = @campaign.release
     @label    = @release.label
     @tracks   = @release.tracks
     
     mail(to:            recipients,
-         subject:       "[Preview] #{campaign.email_subject}",
+         subject:       subject || campaign.email_subject,
          reply_to:      campaign.email_from,
-         template_name: 'campaign')
+         template_name: 'campaign',
+         message_id:    "campaign:#{@campaign.id}+#{SecureRandom.uuid}")
   end
   
 end
