5acb064ca3e91f6371d6b3de8a8e3cc2cd0a881b
Tom-Eric Gerritsen <tomeric@eet.nu>
2013-10-10 12:25:41 +0200

    Use the X-MC-Metadata header to add metadata to the mandrill mails so we can connect it's events to our email logs

--- app/mailers/campaign_mailer.rb
+++ app/mailers/campaign_mailer.rb
@@ -8,16 +8,22 @@ class CampaignMailer < ActionMailer::Base
   end
   
   def normal_campaign(campaign, recipients = [], subject = nil)
-    @campaign = campaign
-    @release  = @campaign.release
-    @label    = @release.label
-    @tracks   = @release.tracks
+    @campaign   = campaign
+    @release    = @campaign.release
+    @label      = @release.label
+    @tracks     = @release.tracks
+    @message_id = "campaign:#{@campaign.id}+#{SecureRandom.uuid}"
     
     mail(to:            recipients,
          subject:       subject || campaign.email_subject,
          reply_to:      campaign.email_from,
          template_name: 'campaign',
-         message_id:    "campaign:#{@campaign.id}+#{SecureRandom.uuid}")
+         message_id:    @message_id)
+    
+    headers['X-MC-Metadata'] = {
+      message_id:  @message_id,
+      campaign_id: @campaign.id
+    }.to_json
   end
   
 end
