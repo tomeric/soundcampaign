f348e067431dc3455413595473aa9f2d05dfc0c0
Tom-Eric Gerritsen <tomeric@eet.nu>
2013-11-17 10:37:30 +0100

    Use a secret_key_base that is set in ".env"

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SoundCampaign::Application.config.secret_key_base = '92b9aead3d640ad412406092ae4d5f8c3f0f2a6232d5b89ec14ab8f6fde1508ab192f5d36c187d6df782f6ff0137f3d349bd19bc0c9617ac5ba812778255729b'
+SoundCampaign::Application.config.secret_key_base = ENV['SECRET_TOKEN']
