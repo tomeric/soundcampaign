91a1b5c76ce90f8170665dc2d8a74ad8088c2734
Tom-Eric Gerritsen <tomeric@eet.nu>
2013-11-18 13:53:13 +0100

    Move paperclip configuration to config/application.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,14 +1,4 @@
 SoundCampaign::Application.configure do
-  # Paperclip & S3:
-  config.paperclip_defaults = {
-    storage: :s3,
-    s3_credentials: {
-      bucket:            ENV['AWS_BUCKET'],
-      access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
-  
   # Code is not reloaded between requests.
   config.cache_classes = true
   
