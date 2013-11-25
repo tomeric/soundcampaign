1779fec7113e6da32549a5070e2e88ba44db50d5
Tom-Eric Gerritsen <tomeric@eet.nu>
2013-11-25 08:45:54 +0100

    Add specs for Campaign#sent? method

--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -4,4 +4,18 @@ describe Campaign do
   subject(:campaign) { build :campaign }
   
   it { should be_valid }
+  
+  describe 'instance methods' do
+    describe '#sent?' do
+      it 'returns true if sent_at is set' do
+        campaign.sent_at = Time.now
+        expect(campaign.sent?).to be_true
+      end
+      
+      it 'returns false if sent_at is nil' do
+        campaign.sent_at = nil
+        expect(campaign.sent?).to be_false
+      end
+    end
+  end
 end
