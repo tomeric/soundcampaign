b0ab1b2b1a8f9df90a732d9c363a36ff21cc4cf0
Tom-Eric Gerritsen <tomeric@eet.nu>
2013-12-03 16:31:36 +0100

    Return a random campaign when doing a campaign mail preview

--- app/mailers/mail_preview.rb
+++ app/mailers/mail_preview.rb
@@ -1,6 +1,6 @@
 class MailPreview < MailView
   def campaign
-    if campaign = Campaign.first
+    if campaign = Campaign.offset(rand(Campaign.count)).first
       recipient = campaign.recipients.first || campaign.recipients.new(email: 'john.doe@example.com')
       CampaignMailer.normal_campaign(campaign, recipient)
     end
