68dfc4f2315c00b314c40e437b99e5313f4f973f
Tom-Eric Gerritsen <tomeric@eet.nu>
2013-12-07 09:44:39 +0100

    Add some whitespace to GravatarHelper

--- app/helpers/gravatar_helper.rb
+++ app/helpers/gravatar_helper.rb
@@ -1,17 +1,22 @@
-    require 'digest/md5'
+require 'digest/md5'
+
 module GravatarHelper
  def gravatar_tag(email, *args)
     opts = args.extract_options!
-    opts[:class]||=""
-    opts[:class]+=" gravatar"
+    
+    opts[:class] ||= ""
+    opts[:class]  += " gravatar"
+    
     size = opts.delete(:size) || 80
     
-    default=""
+    default = ""
+    
     if opts[:default]
       require 'cgi'
       default= "&d=#{CGI::escape(opts.delete(:default))}"
     end
-    hash = Digest::MD5.hexdigest(email.downcase)    
+    
+    hash = Digest::MD5.hexdigest(email.downcase)
     
     image_tag "http://www.gravatar.com/avatar/#{hash}?size=#{size}#{default}", opts
   end
