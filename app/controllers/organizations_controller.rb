5ab594d50a630603510dec6385866bf512a1564f
Tom-Eric Gerritsen <tomeric@eet.nu>
2013-12-12 11:59:38 +0100

    Add charts and stuff to the /organizations page

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -4,6 +4,18 @@ class OrganizationsController < ApplicationController
   
   def index
     @organizations = Organization.order(created_at: :desc).all
+    
+    # Counters on top of page:
+    @recipient_count   = Recipient.count
+    @feedback_count    = Feedback.count
+    @open_count        = EmailLog.opened.count
+    @open_today_count  = EmailLog.opened(Date.today).count
+    @plays_count       = TrackEvent.played.count
+    @plays_today_count = TrackEvent.played(Date.today).count
+    
+    # Graphs:
+    @opens_per_day = EmailLog.number_of   :opens, per: 1.day
+    @plays_per_day = TrackEvent.number_of :plays, per: 1.day
   end
   
 end
