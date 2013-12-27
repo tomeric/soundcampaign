8ee64fa364b6fe521ba6f91f6cfa34affb572244
Tom-Eric Gerritsen <tomeric@eet.nu>
2013-12-27 13:59:03 +0100

    Add helper methods to help with setting the value of the <title/> element

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,31 @@
 module ApplicationHelper
+  def site_name
+    Settings['name']
+  end
+  
+  def site_subtitle
+    Settings['subtitle']
+  end
+  
+  def title(new_title = nil)
+    if new_title.present?
+      @page_title = new_title
+    else
+      @page_title || ""
+    end
+  end
+  
+  def page_title
+    if title.present?
+      parts = [title, site_name]
+    else
+      parts = [site_name, site_subtitle]
+    end
+    
+    page_title = strip_tags parts.compact.join(' — ')
+    page_title.html_safe
+  end
+  
   def flash_messages
     return unless flash.present?
     
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,6 +1,55 @@
 require 'spec_helper'
 
 describe ApplicationHelper do
+  describe '#page_title' do
+    it 'includes the title at the start' do
+      helper.title 'Page Title'
+      expect(helper.page_title).to start_with 'Page Title — '
+    end
+    
+    it 'strips HTML tags from the title' do
+      helper.title '<a>Page</a> Title'
+      expect(helper.page_title).to match /^Page Title — /
+    end
+    
+    it 'includes the site_name at the end' do
+      helper.stub(:site_name).and_return('Site Name')
+      helper.title 'Page Title'
+      expect(helper.page_title).to end_with 'Site Name'
+    end
+    
+    it 'returns the site_name and the site_subtitle if no page_title is set' do
+      helper.stub(:site_name).and_return('Site Name')
+      helper.stub(:site_subtitle).and_return('Site Subtitle')
+      expect(helper.page_title).to eql 'Site Name — Site Subtitle'
+    end
+  end
+  
+  describe '#site_name' do
+    it 'returns the configured name' do
+      Settings.stub(:[]).with('name').and_return('Site Name')
+      expect(helper.site_name).to eql 'Site Name'
+    end
+  end
+  
+  describe '#site_subtitle' do
+    it 'returns the configured subtitle' do
+      Settings.stub(:[]).with('subtitle').and_return('Site Subtitle')
+      expect(helper.site_subtitle).to eql 'Site Subtitle'
+    end
+  end
+  
+  describe '#title' do
+    it 'stores the title and return it back' do
+      helper.title 'Test title'
+      expect(helper.title).to eql 'Test title'
+    end
+    
+    it 'returns an empty string if no title is set' do
+      expect(helper.title).to eql ''
+    end
+  end
+  
   describe '#flash_messages' do
     before do
       helper.stub(:flash)
