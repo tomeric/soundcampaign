9683482f40fcbe95e63257cc3f64054939b32810
Tom-Eric Gerritsen <tomeric@eet.nu>
2014-12-03 19:34:09 +0100

    Updated deployment for 'whenever'

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,3 @@
-require 'whenever/capistrano'
-
 set :application, 'soundcampaign'
 set :rails_env,   'production'
 
@@ -26,6 +24,9 @@ set :rvm_ruby_version, '2.1.0'
 # Delayed Job:
 set :delayed_job_workers, 2
 
+# Whenever:
+set :whenever_roles, -> { :worker }
+
 # Execute bundled binaries with 'bundle exec', execute global ruby binaries with 'rvm x.x.x do':
 SSHKit.config.command_map = Hash.new do |hash, key|
   rvm_prepend     = "#{fetch(:rvm_path)}/bin/rvm #{fetch(:rvm_ruby_version)} do"
@@ -33,7 +34,7 @@ SSHKit.config.command_map = Hash.new do |hash, key|
   
   if %w{rails rake script/delayed_job}.include?(key.to_s)
     hash[key] = "#{rvm_prepend} #{bundle_prepend} #{key}"
-  elsif %w{erb bundle backup script/unicorn}.include?(key.to_s)
+  elsif %w{erb bundle backup script/unicorn whenever}.include?(key.to_s)
     hash[key] = "#{rvm_prepend} #{key}"
   else
     hash[key] = key
@@ -51,6 +52,11 @@ set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle publi
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 # set :keep_releases, 5
 
+namespace :whenever do
+  before :update_crontab, 'rvm:init'
+  before :clear_crontab,  'rvm:init'
+end
+
 namespace :deploy do
   desc 'Setup application'
   task :setup do
@@ -63,7 +69,6 @@ namespace :deploy do
   
   desc 'Cleanup'
   task :cleanup do
-    invoke :'rvm:init'
     on primary(:app) do
       within(release_path) { execute :rake, "honeybadger:deploy TO=#{fetch(:rails_env)} REPO=#{repo_url} USER=#{local_user}" }
     end
